d9e0fee4d718a910f1072df71e0a57a113a2e5f3
mlazarevich <priobit.email@gmail.com>
2020-06-30 21:57:56 +0300

    Client BO has been created.

---
+++ src/com/skillsimprover/cafeapp/bo/ClientBo.java
@@ -0,0 +1,14 @@
+package com.skillsimprover.cafeapp.bo;
+
+public class ClientBo {
+
+    String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}

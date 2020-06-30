424dfbdf5e0876d7e09043626551cb7bff1b87da
mlazarevich <priobit.email@gmail.com>
2020-06-30 22:14:25 +0300

    UI of the main window has been changed.

--- src/com/skillsimprover/cafeapp/Main.java
+++ src/com/skillsimprover/cafeapp/Main.java
@@ -1,8 +1,14 @@
 package com.skillsimprover.cafeapp;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello My Cafe!");
+
+        Random random = new Random();
+
+        System.out.println("Welcome to our cafe");
+        System.out.println("Soup of the day only for " + random.nextInt(100) + "$");
     }
 }
